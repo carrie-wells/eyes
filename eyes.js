463a01676216eba0262a8073782c403faf01396c
Carrie Wells <108761547+carrie-wells@users.noreply.github.com>
2022-09-25 19:59:19 -0400

    Add files via upload

---
+++ eyes.js
@@ -0,0 +1,18 @@
+const irises = document.getElementsByClassName('iris');
+const pupils = document.getElementsByClassName('pupil');
+  
+document.onmousemove = (event) => {
+    const x = (event.clientX * 100) / window.innerWidth + '%';
+    const y = (event.clientY * 100) / window.innerHeight + '%';
+
+for (let i = 0; i < 2; i++) {
+    irises[i].style.left = x;
+    irises[i].style.top = y;
+    irises[i].transform = 'translate(-' + x + ',-' + y + ')';
+}
+for (let i = 0; i < 2; i++) {
+  pupils[i].style.left = x;
+  pupils[i].style.top = y;
+  pupils[i].transform = 'translate(-' + x + ',-' + y + ')';
+  };
+}
